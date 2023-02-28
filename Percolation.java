7d520c984615952bc4203b802e27ae9eae1f0f0a
PatriotRossii <patriotrossii2019@mail.ru>
2023-02-28 22:00:16 +0300

    Implement handling of corner cases in Percolation methods

--- Percolation.java
+++ Percolation.java
@@ -6,13 +6,9 @@ public class Percolation {
     private WeightedQuickUnionUF union;
     private boolean[] isSiteOpen;
 
-
-    private int getSiteAddress(int row, int column) {
-        return (row - 1) * n + (column - 1);
-    }
-
     // create n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException();
         this.n = n;
 
         int totalElements = n * n;
@@ -32,12 +28,19 @@ public class Percolation {
         }
     }
 
+    private int getSiteAddress(int row, int column) {
+        return (row - 1) * n + (column - 1);
+    }
+
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
+        if ((row < 1 || row > n) || (col < 1 || col > n))
+            throw new IllegalArgumentException();
+
         if (!isOpen(row, col)) openSites += 1;
         isSiteOpen[getSiteAddress(row, col)] = true;
 
-        if (row == 0) union.union(virtualTop, getSiteAddress(row, col));
+        if (row == 1) union.union(virtualTop, getSiteAddress(row, col));
         if (row == n) union.union(virtualBottom, getSiteAddress(row, col));
 
         if (row > 1 && isOpen(row - 1, col)) {
@@ -56,11 +59,17 @@ public class Percolation {
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
+        if ((row < 1 || row > n) || (col < 1 || col > n))
+            throw new IllegalArgumentException();
+
         return isSiteOpen[getSiteAddress(row, col)];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
+        if ((row < 1 || row > n) || (col < 1 || col > n))
+            throw new IllegalArgumentException();
+
         return isOpen(row, col) && (union.find(virtualTop) == union.find(getSiteAddress(row, col)));
     }
 
